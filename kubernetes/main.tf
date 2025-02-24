9b01dbfa61b7744f2e387c1518b3b691ed42a5b2
Yashraj Gosavi <yashrajgosavi2580@gmail.com>
2025-02-24 18:10:57 +0000

    feat: resource namespace created

--- kubernetes/main.tf
+++ kubernetes/main.tf
@@ -11,3 +11,9 @@ provider "kubernetes" {
   config_path = "~/.kube/config"  # Path to your kubeconfig file
   config_context = "minikube"
 }
+
+resource "kubernetes_namespace" "namespace-test" {
+  metadata {
+    name = "test"
+  }
+}
